a999df906d20ed10da3369f13f35f990f1cea8d9
Pratibha <pratibha.jadhav3229@gmail.com>
2022-07-11 08:35:51 +0700

    initial setup of app

---
+++ practiceApp/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TemplateFormsComponent } from './template-forms/template-forms.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TemplateFormsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
