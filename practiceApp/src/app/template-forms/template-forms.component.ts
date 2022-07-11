85c079c6e2a1ac61318501e931cf95e989234169
Pratibha <pratibha.jadhav3229@gmail.com>
2022-07-11 13:18:10 +0700

    template driven programs

--- practiceApp/src/app/template-forms/template-forms.component.ts
+++ practiceApp/src/app/template-forms/template-forms.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./template-forms.component.css']
 })
 export class TemplateFormsComponent implements OnInit {
+  title = "Template Driven Forms";
+
+  countryList: Country[] = [
+    new Country("1", "India"),
+    new Country("2", "Thailand"),
+    new Country("3", "Germany")
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
   }
+  onSubmit(contactForm:any){
+    console.log(contactForm.value);
+  }
 
+}
+export class Country{
+  id:string;
+  name:string;
+
+  constructor(id:string, name:string){
+    this.id = id;
+    this.name = name;
+  }
 }
